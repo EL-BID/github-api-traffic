453b26cafb8f56bd0ba93472367af00fdf3f7e7b
Jose Cortes <jlcmux@gmail.com>
2022-10-20 18:34:50 -0500

    sched

--- scheduler.py
+++ scheduler.py
@@ -2,20 +2,19 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from updates import UpdateRepos
 from app import app
 
-app.app_context()
+with app.app_context():
+
+    class Config:
+        SCHEDULER_API_ENABLED = True
 
 
-class Config:
-    SCHEDULER_API_ENABLED = True
+    scheduler = BlockingScheduler()
 
 
-scheduler = BlockingScheduler()
+    @scheduler.scheduled_job('interval', minutes=3)
+    def job1():
+        out = UpdateRepos()
+        print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=3)
-def job1():
-    out = UpdateRepos()
-    print(out)
-
-
-scheduler.start()
+    scheduler.start()
