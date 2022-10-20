aee290097e345cf3e5a2c49c897c576d444f262d
Jose Cortes <jlcmux@gmail.com>
2022-10-20 18:44:14 -0500

    sched

--- updates.py
+++ updates.py
@@ -1,11 +1,12 @@
 from db import db, Repo
 from utils.scripts import GetTraffic, GetAccessToken, GetInstallations, GetRepos
 import json
+from app import app
 
-
-def UpdateRepos():
-    repos = GetRepos()
-    for repo in repos:
-        if not Repo.query.filter_by(name=repo["name"]).first():
-            db.session.add(Repo(repo["name"]))
-            db.session.commit()
+with app.app_context():
+    def UpdateRepos():
+        repos = GetRepos()
+        for repo in repos:
+            if not Repo.query.filter_by(name=repo["name"]).first():
+                db.session.add(Repo(repo["name"]))
+                db.session.commit()
