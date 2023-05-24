5f299ac5e41ece79d0878107815e73a3c96f6646
Jose Cortes <jose.cortes@resolute-panam.com>
2023-05-24 18:31:16 -0500

    test scheduler

--- scheduler.py
+++ scheduler.py
@@ -5,7 +5,13 @@ from app import app
 
 with app.app_context():
 
+    class Config:
+        SCHEDULER_API_ENABLED = True
 
+
+    scheduler = BlockingScheduler()
+
+    @scheduler.scheduled_job('interval', hours=24)
     def job1():
         out = UpdateRepos()
         print(out)
@@ -13,39 +19,40 @@ with app.app_context():
 
 
     #schedule every minute for testing
-
+    @scheduler.scheduled_job('interval', hours=24)
     def job2():
         out = UpdateClonesSummary()
         print(out)
 
 
+    @scheduler.scheduled_job('interval', hours=24)
     def job3():
         out = UpdateClonesHistory()
         print(out)
 
 
-
+    @scheduler.scheduled_job('interval', hours=24)
     def job4():
         out = UpdateViewsSummary()
         print(out)
 
 
-
+    @scheduler.scheduled_job('interval', hours=24)
     def job5():
         out = UpdateViewsHistory()
         print(out)
 
 
+    @scheduler.scheduled_job('interval', hours=24)
     def job6():
         out = UpdateRefSources()
         print(out)
 
 
-
+    @scheduler.scheduled_job('interval', hours=24)
     def job7():
         out = UpdatePaths()
         print(out)
 
 
-    job7()
-
+    scheduler.start()
