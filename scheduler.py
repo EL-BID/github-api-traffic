22da7a275448dd165ed8ec673cfe8357aac249d2
Jose Cortes <jose.cortes@resolute-panam.com>
2023-06-13 20:06:48 -0500

    interval change

--- scheduler.py
+++ scheduler.py
@@ -11,44 +11,44 @@ with app.app_context():
     scheduler = BlockingScheduler()
 
 
-    @scheduler.scheduled_job('interval', hours=20)
+    @scheduler.scheduled_job('interval', hours=1)
     def job1():
         out = UpdateRepos()
         print(out)
 
 
     # schedule every minute for testing
-    @scheduler.scheduled_job('interval', hours=21)
+    @scheduler.scheduled_job('interval', hours=2)
     def job2():
         out = UpdateClonesSummary()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=22)
+    @scheduler.scheduled_job('interval', hours=3)
     def job3():
         out = UpdateClonesHistory()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=23)
+    @scheduler.scheduled_job('interval', hours=4)
     def job4():
         out = UpdateViewsSummary()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=24)
+    @scheduler.scheduled_job('interval', hours=5)
     def job5():
         out = UpdateViewsHistory()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=25)
+    @scheduler.scheduled_job('interval', hours=6)
     def job6():
         out = UpdateRefSources()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=26)
+    @scheduler.scheduled_job('interval', hours=7)
     def job7():
         out = UpdatePaths()
         print(out)
