f8f99c45be9281f63314be2dc93ca9ae299f44f2
Jose Cortes <jose.cortes@resolute-panam.com>
2023-06-13 20:32:43 -0500

    sched

--- scheduler.py
+++ scheduler.py
@@ -3,51 +3,54 @@ from updates import UpdateRepos, UpdateClonesSummary, UpdateClonesHistory, Updat
     UpdateRefSources, UpdatePaths
 from app import app
 
-with app.app_context():
-    scheduler = BlockingScheduler()
+def app_context():
+    with app.app_context():
+        yield
+
+scheduler = BlockingScheduler()
 
 
-    @scheduler.scheduled_job('interval', minutes=1)
-    def job1():
-        out = UpdateRepos()
-        print(out)
+@scheduler.scheduled_job('interval', minutes=1)
+def job1(app_context):
+    out = UpdateRepos()
+    print(out)
 
 
-    # schedule every minute for testing
-    @scheduler.scheduled_job('interval', minutes=2)
-    def job2():
-        out = UpdateClonesSummary()
-        print(out)
+
+@scheduler.scheduled_job('interval', minutes=2)
+def job2(app_context):
+    out = UpdateClonesSummary()
+    print(out)
 
 
-    @scheduler.scheduled_job('interval', minutes=3)
-    def job3():
-        out = UpdateClonesHistory()
-        print(out)
+@scheduler.scheduled_job('interval', minutes=3)
+def job3(app_context):
+    out = UpdateClonesHistory()
+    print(out)
 
 
-    @scheduler.scheduled_job('interval', minutes=4)
-    def job4():
-        out = UpdateViewsSummary()
-        print(out)
+@scheduler.scheduled_job('interval', minutes=4)
+def job4(app_context):
+    out = UpdateViewsSummary()
+    print(out)
 
 
-    @scheduler.scheduled_job('interval', minutes=5)
-    def job5():
-        out = UpdateViewsHistory()
-        print(out)
+@scheduler.scheduled_job('interval', minutes=5)
+def job5(app_context):
+    out = UpdateViewsHistory()
+    print(out)
 
 
-    @scheduler.scheduled_job('interval', minutes=6)
-    def job6():
-        out = UpdateRefSources()
-        print(out)
+@scheduler.scheduled_job('interval', minutes=6)
+def job6(app_context):
+    out = UpdateRefSources()
+    print(out)
 
 
-    @scheduler.scheduled_job('interval', minutes=7)
-    def job7():
-        out = UpdatePaths()
-        print(out)
+@scheduler.scheduled_job('interval', minutes=7)
+def job7(app_context):
+    out = UpdatePaths()
+    print(out)
 
 
-    scheduler.start()
+scheduler.start()
