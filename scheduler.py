2af0cc633208d19d2b7136c4cda8e6b29532a40b
Jose Cortes <jose.cortes@resolute-panam.com>
2023-06-13 20:25:16 -0500

    sched

--- scheduler.py
+++ scheduler.py
@@ -7,44 +7,44 @@ with app.app_context():
     scheduler = BlockingScheduler()
 
 
-    @scheduler.scheduled_job('interval', hours=1)
+    @scheduler.scheduled_job('interval', minutes=1)
     def job1():
         out = UpdateRepos()
         print(out)
 
 
     # schedule every minute for testing
-    @scheduler.scheduled_job('interval', hours=2)
+    @scheduler.scheduled_job('interval', minutes=2)
     def job2():
         out = UpdateClonesSummary()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=3)
+    @scheduler.scheduled_job('interval', minutes=3)
     def job3():
         out = UpdateClonesHistory()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=4)
+    @scheduler.scheduled_job('interval', minutes=4)
     def job4():
         out = UpdateViewsSummary()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=5)
+    @scheduler.scheduled_job('interval', minutes=5)
     def job5():
         out = UpdateViewsHistory()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=6)
+    @scheduler.scheduled_job('interval', minutes=6)
     def job6():
         out = UpdateRefSources()
         print(out)
 
 
-    @scheduler.scheduled_job('interval', hours=7)
+    @scheduler.scheduled_job('interval', minutes=7)
     def job7():
         out = UpdatePaths()
         print(out)
