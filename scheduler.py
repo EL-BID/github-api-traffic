04f55168837925c192d6a1ac3d6099ddceeb1184
Jose Cortes <jose.cortes@resolute-panam.com>
2023-06-13 20:38:14 -0500

    sched

--- scheduler.py
+++ scheduler.py
@@ -4,55 +4,64 @@ from updates import UpdateRepos, UpdateClonesSummary, UpdateClonesHistory, Updat
 from app import app
 from flask import Flask
 
-def app_context():
-    app = Flask(__name__)
-    with app.app_context():
-        yield
-
 scheduler = BlockingScheduler()
 
 
 @scheduler.scheduled_job('interval', minutes=1)
-def job1(app_context):
-    out = UpdateRepos()
-    print(out)
+def job1():
+    app = Flask(__name__)
+    with app.app_context():
+        out = UpdateRepos()
+        print(out)
 
 
 
 @scheduler.scheduled_job('interval', minutes=2)
-def job2(app_context):
-    out = UpdateClonesSummary()
-    print(out)
+def job2():
+    app = Flask(__name__)
+    with app.app_context():
+        out = UpdateClonesSummary()
+        print(out)
 
 
 @scheduler.scheduled_job('interval', minutes=3)
-def job3(app_context):
-    out = UpdateClonesHistory()
-    print(out)
+def job3():
+    app = Flask(__name__)
+    with app.app_context():
+        out = UpdateClonesHistory()
+        print(out)
 
 
 @scheduler.scheduled_job('interval', minutes=4)
-def job4(app_context):
-    out = UpdateViewsSummary()
-    print(out)
+def job4():
+    app = Flask(__name__)
+    with app.app_context():
+        out = UpdateViewsSummary()
+        print(out)
 
 
 @scheduler.scheduled_job('interval', minutes=5)
-def job5(app_context):
-    out = UpdateViewsHistory()
-    print(out)
+def job5():
+    app = Flask(__name__)
+    with app.app_context():
+        out = UpdateViewsHistory()
+        print(out)
 
 
 @scheduler.scheduled_job('interval', minutes=6)
-def job6(app_context):
-    out = UpdateRefSources()
-    print(out)
+def job6():
+    app = Flask(__name__)
+    with app.app_context():
+        out = UpdateRefSources()
+        print(out)
 
 
 @scheduler.scheduled_job('interval', minutes=7)
-def job7(app_context):
-    out = UpdatePaths()
-    print(out)
+def job7():
+    app = Flask(__name__)
+    with app.app_context():
+        out = UpdatePaths()
+        print(out)
 
 
 scheduler.start()
