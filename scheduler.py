73065303f225deda4e0ad6cda8ec7a90c3125401
Jose Cortes <jose.cortes@resolute-panam.com>
2023-06-21 11:05:32 -0500

    sched

--- scheduler.py
+++ scheduler.py
@@ -7,7 +7,7 @@ from flask import Flask
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=1)
 def job1():
     print("job1")
     with app.app_context():
@@ -16,7 +16,7 @@ def job1():
 
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=1)
 def job2():
     print("job2")
     with app.app_context():
@@ -24,7 +24,7 @@ def job2():
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=1)
 def job3():
     print("job3")
     with app.app_context():
@@ -32,7 +32,7 @@ def job3():
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=1)
 def job4():
     print("job4")
     with app.app_context():
@@ -40,7 +40,7 @@ def job4():
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=1)
 def job5():
     print("job5")
     with app.app_context():
@@ -48,7 +48,7 @@ def job5():
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=1)
 def job6():
     print("job6")
     with app.app_context():
@@ -56,14 +56,14 @@ def job6():
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=1)
 def job7():
     print("job7")
     with app.app_context():
         out = UpdatePaths()
         print(out)
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=1)
 def job8():
     print("job8")
     with app.app_context():
