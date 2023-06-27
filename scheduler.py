99761c5debd5592f8d9c7d15211abce1d4abe3cd
Jose Cortes <jose.cortes@resolute-panam.com>
2023-06-27 18:20:22 -0500

    test minutes

--- scheduler.py
+++ scheduler.py
@@ -15,48 +15,48 @@ def job1():
 
 
 
-@scheduler.scheduled_job('interval', minutes=1)
+@scheduler.scheduled_job('interval', hours=12)
 def job2():
     with app.app_context():
         out = UpdateClonesSummary()
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=2)
+@scheduler.scheduled_job('interval', hours=13)
 def job3():
     with app.app_context():
         out = UpdateClonesHistory()
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=3)
+@scheduler.scheduled_job('interval', hours=14)
 def job4():
     with app.app_context():
         out = UpdateViewsSummary()
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=4)
+@scheduler.scheduled_job('interval', hours=15)
 def job5():
     with app.app_context():
         out = UpdateViewsHistory()
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', hours=16)
 def job6():
     with app.app_context():
         out = UpdateRefSources()
         print(out)
 
 
-@scheduler.scheduled_job('interval', minutes=6)
+@scheduler.scheduled_job('interval', hours=17)
 def job7():
     with app.app_context():
         out = UpdatePaths()
         print(out)
 
-@scheduler.scheduled_job('interval', minutes=7)
+@scheduler.scheduled_job('interval', hours=18)
 def job8():
     with app.app_context():
         out = UpdateForks()
